64be3218d5ee9f0dae603ef462092f0c7cb50160
Mariam Alhmidi <m.alhmidi@calani.net>
2022-06-23 12:53:04 +0200

    first projekt

---
+++ MAHBook/MAHBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,54 @@
+﻿using MAHBookWeb.Data;
+using MAHBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace MAHBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category>objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+         // Get Action Methode
+
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        // Post
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            
+        }
+
+    }
+}
---
+++ MAHBook/MAHBookWeb/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MAHBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID_Category { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
